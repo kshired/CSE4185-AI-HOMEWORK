9921ec24339b8a0a133eec41bf5023c3de4c5aa6
kshired <shiroed1211@naver.com>
2021-09-14 11:53:23 +0900

    add comment

--- Assignment01/search.py
+++ Assignment01/search.py
@@ -29,10 +29,14 @@ def bfs(maze):
     # Tracing을 위한 Set
     prev = {}
 
+    # BFS를 위한 큐
     queue = deque()
     queue.append(start_point)
+
+    # 이미 방문한 좌표인지 체크하기위한 visit 배열
     visit = [start_point]
     
+    # BFS
     while queue:
         y,x = queue.popleft()
         if (y,x) == end_point:
@@ -51,6 +55,7 @@ def bfs(maze):
         node = prev[node]
         path.append(node)
 
+    # Check Path is valid
     isValidPath(path)
     return path[::-1]
     ############################################################################
@@ -104,31 +109,39 @@ def astar(maze):
     path=[]
 
     ####################### Write Your Code Here ################################
+    # Start and End (Objective) Node
     start = Node(None, start_point)
     end = Node(None, end_point)
 
+    # Open and Close List
     open = []
     close = []
+
+    # Heapify Open List
     hq.heappush(open,start)
 
+    # A*
     while open:
         cur_node = hq.heappop(open)
         close.append(cur_node)
 
+        # End에 도착하면 Path를 찾고 break
         if cur_node == end:
+            # Trace Path
             cur = cur_node
             while cur:
                 path.append(cur.location)
                 cur = cur.parent
             break
         
-
+        
+        # Find Next Open Nodes
         for dy,dx in maze.neighborPoints(cur_node.location[0],cur_node.location[1]):
             new_node = Node(cur_node,(dy,dx))
             if new_node in close:
                 continue
             new_node.g = cur_node.g + 1
-            new_node.h = manhatten_dist(new_node.location, end.location)
+            new_node.h = manhatten_dist(new_node.location, end.location) # heuristic == manhatten_dist
             new_node.f = new_node.g + new_node.h
 
             for value in open:
@@ -137,8 +150,11 @@ def astar(maze):
             else:
                 hq.heappush(open,new_node)
 
-                
+    
+    # Reverse Path
     path = path[::-1]
+
+    # Check Path is valid
     isValidPath(path)
     return path
 
@@ -150,6 +166,7 @@ def astar(maze):
 
 
 def stage2_heuristic(cur, end, visit):
+    # 현재 좌표에서 방문하지 않은 좌표들과의 맨해튼 거리 중 가장 작은 맨해튼 거리를 return
     min_value = float("inf")
     for idx,end_point in enumerate(end):
         if not visit[idx]:
@@ -171,39 +188,55 @@ def astar_four_circles(maze):
     ####################### Write Your Code Here ################################
     start_point = maze.startPoint()
 
+    # Start Node and End Nodes (Objectives)
     start = Node(None, start_point)
     end = [Node(None, end_point) for end_point in end_points]
+
+    # 도착 좌표를 방문했는지 체크하기위한 visit 배열
     visit = [False for _ in range(len(end_points))]
 
     for _ in range(4):
+        # Initialize Open and Close List at every iteration
         open = []
         close = []
+
+        # iteration이 처음인 경우 == 여태까지 구한 path가 없는 경우
         if len(path) != 0:
             hq.heappush(open,Node(None,path[-1]))
-        else:
+        else:   # 구한 path가 있다면, 마지막 지점부터 다시 시작
             hq.heappush(open,start)
+
+        # A*
         while open:
             cur_node = hq.heappop(open)
             close.append(cur_node)
 
+            # 현재 좌표가 도착 좌표들 중 하나라면 path를 추가
             if cur_node in end:
                 if not visit[end.index(cur_node)]: 
+                    # 도착 좌표를 방문했다고 체크
                     visit[end.index(cur_node)] = True
+
+                    # Path Trace
                     cur = cur_node
                     tmp = []
                     while cur:
                         tmp.append(cur.location)
                         cur = cur.parent
+                    
+                    # i-1번째 iteration의 마지막 좌표 ==  i번째 iteration의 첫 좌표
+                    # 그렇기에 이전 path에서 [:-1]까지 가져오고 현재 path(tmp)를 reverse하여 추가
                     path = path[:-1] + tmp[::-1]
                     break
 
-
+            
+            # Find Next Open Nodes
             for dy,dx in maze.neighborPoints(cur_node.location[0],cur_node.location[1]):
                 new_node = Node(cur_node,(dy,dx))
                 if new_node in close:
                     continue
                 new_node.g = cur_node.g + 1
-                new_node.h = stage2_heuristic(new_node.location, end ,visit)
+                new_node.h = stage2_heuristic(new_node.location, end ,visit) # heuristic == stage2_heuristic
                 new_node.f = new_node.g + new_node.h
 
                 for value in open:
@@ -212,6 +245,7 @@ def astar_four_circles(maze):
                 else:
                     hq.heappush(open,new_node)
 
+    # Check Path is valid
     isValidPath(path)
     return path
     ############################################################################
@@ -232,6 +266,7 @@ def mst(start, objectives):
     
     graph = defaultdict(list)
 
+    # 방문하지않은 각 도착 좌표간의 맨해튼 거리를 구한다
     for y1,x1 in objectives:
         for y2,x2 in objectives:
             if y1 == y2 and x1 == x2:
@@ -240,11 +275,13 @@ def mst(start, objectives):
             graph[(y1,x1)].append((weight, (y2,x2)))
             graph[(y2,x2)].append((weight, (y1,x1)))
     
+    # 현재 좌표와 방문하지않은 각 도착 좌표사이의 맨해튼 거리를 구한다
     for y,x in objectives:
         weight = manhatten_dist(start,(y,x))
         graph[(y,x)].append((weight,start))
         graph[start].append((weight,(y,x)))
 
+    # MST 만들기 ( Prim 알고리즘 )
     connected = set([start])
     candidate_edge = graph[start]
     hq.heapify(candidate_edge)
@@ -289,39 +326,55 @@ def astar_many_circles(maze):
     ####################### Write Your Code Here ################################
     start_point = maze.startPoint()
 
+    # Start Node and End Nodes (Objectives)
     start = Node(None, start_point)
     end = [Node(None, end_point) for end_point in end_points]
+
+    # 도착 좌표를 방문했는지 체크하기위한 visit 배열
     visit = [False for _ in range(len(end_points))]
 
+    # 모든 도착 좌표를 방문하지 않았을 때까지 iteration
     while visit.count(True) < len(visit):
+        # Initialize Open and Close List at every iteration
         open = []
         close = []
+
+        # iteration이 처음인 경우 == 여태까지 구한 path가 없는 경우
         if len(path) != 0:
             hq.heappush(open,Node(None,path[-1]))
-        else:
+        else:   # 구한 path가 있다면, 마지막 지점부터 다시 시작
             hq.heappush(open,start)
+        
+        # A* 알고리즘
         while open:
             cur_node = hq.heappop(open)
             close.append(cur_node)
 
+            # 현재 좌표가 도착 좌표들 중 하나라면 path를 추가
             if cur_node in end:
                 if not visit[end.index(cur_node)]: 
+                    # 도착 좌표를 방문했다고 체크
                     visit[end.index(cur_node)] = True
+
+                    # Path Trace
                     cur = cur_node
                     tmp = []
                     while cur:
                         tmp.append(cur.location)
                         cur = cur.parent
+                    
+                    # i-1번째 iteration의 마지막 좌표 ==  i번째 iteration의 첫 좌표
+                    # 그렇기에 이전 path에서 [:-1]까지 가져오고 현재 path(tmp)를 reverse하여 추가
                     path = path[:-1] + tmp[::-1]
                     break
 
-
+            # Find Next Open Nodes
             for dy,dx in maze.neighborPoints(cur_node.location[0],cur_node.location[1]):
                 new_node = Node(cur_node,(dy,dx))
                 if new_node in close:
                     continue
                 new_node.g = cur_node.g + 1
-                new_node.h = stage3_heuristic(new_node.location, end ,visit)
+                new_node.h = stage3_heuristic(new_node.location, end ,visit) # heurisitc == stage3_heuristic
                 new_node.f = new_node.g + new_node.h
 
                 for value in open:
@@ -330,10 +383,13 @@ def astar_many_circles(maze):
                 else:
                     hq.heappush(open,new_node)
                     
+    # Check Path is Valid
     isValidPath(path)
     return path
 
     ############################################################################
+
+# Check pos1 and pos2 are neighbor
 def isNeighbor(pos1,pos2):
     if abs(pos1[0] - pos2[0]) == 0 and abs(pos1[1]-pos2[1]) == 1:
         return True
@@ -341,7 +397,9 @@ def isNeighbor(pos1,pos2):
         return True
     return False
 
+# Check Path is valid
 def isValidPath(path):
+    # 인접한 경로의 좌표가 neighbor가 아니라면 raise Error
     tmp = path[0]
     cnt = 0
     for y,x in path[1:]:
